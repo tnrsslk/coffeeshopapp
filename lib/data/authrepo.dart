f8d9e45d76884438f00262316e78037b72400ee9
tnrsslk <156302093+tnrsslk@users.noreply.github.com>
2025-01-29 02:55:44 +0500

    Your commit message

--- lib/data/authrepo.dart
+++ lib/data/authrepo.dart
@@ -1,16 +1,27 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 
 class AuthRepository {
-  AuthRepository(FirebaseApp app) : _auth = FirebaseAuth.instanceFor(app: app);
+  AuthRepository(FirebaseApp app, {required FirebaseAnalytics analytics})
+      : _auth = FirebaseAuth.instanceFor(app: app),
+        _analytics = analytics;
+
   final FirebaseAuth _auth;
+  final FirebaseAnalytics _analytics; // Добавляем для Firebase Analytics
   String _name = "";
+
   bool get isSignedIn => _auth.currentUser != null;
+
   Future<(bool, String?)> signIn(String email, String password) async {
     try {
       await _auth.signInWithEmailAndPassword(email: email, password: password);
       _name = _auth.currentUser?.displayName ?? "";
+      // Логирование в Firebase Analytics
+      await _analytics.logEvent(name: 'user_sign_in', parameters: {
+        'email': email,
+      });
       return (true, null);
     } catch (e) {
       FirebaseAuthException error = e as FirebaseAuthException;
@@ -25,14 +36,21 @@ class AuthRepository {
   Future<void> changeName(String name) async {
     await _auth.currentUser?.updateDisplayName(name);
     _name = name;
+    // Логирование изменения имени в Firebase Analytics
+    await _analytics.logEvent(name: 'user_change_name', parameters: {
+      'name': name,
+    });
   }
 
-  Future<(bool, String?)> signUp(
-      String email, String password, String name) async {
+  Future<(bool, String?)> signUp(String email, String password, String name) async {
     try {
-      await _auth.createUserWithEmailAndPassword(
-          email: email, password: password);
+      await _auth.createUserWithEmailAndPassword(email: email, password: password);
       await _auth.currentUser?.updateDisplayName(name);
+      // Логирование регистрации пользователя в Firebase Analytics
+      await _analytics.logEvent(name: 'user_sign_up', parameters: {
+        'email': email,
+        'name': name,
+      });
       return (true, null);
     } catch (e) {
       FirebaseAuthException error = e as FirebaseAuthException;
@@ -42,6 +60,8 @@ class AuthRepository {
 
   Future<void> signOut() async {
     await _auth.signOut();
+    // Логирование выхода из приложения в Firebase Analytics
+    await _analytics.logEvent(name: 'user_sign_out');
   }
 
   Future<bool> deleteAccount(String pass) async {
@@ -49,11 +69,10 @@ class AuthRepository {
       AuthCredential credential = EmailAuthProvider.credential(
           email: _auth.currentUser?.email ?? "", password: pass);
       await _auth.currentUser?.reauthenticateWithCredential(credential);
-      await FirebaseFirestore.instance
-          .collection("users")
-          .doc(getUid())
-          .delete();
+      await FirebaseFirestore.instance.collection("users").doc(getUid()).delete();
       await _auth.currentUser?.delete();
+      // Логирование удаления аккаунта в Firebase Analytics
+      await _analytics.logEvent(name: 'user_delete_account');
       return true;
     } catch (e) {
       return false;
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
-// ignore_for_file: use_key_in_widget_constructors
-
 import 'package:coffeeshopapp/blocs/cart_bloc.dart';
 import 'package:coffeeshopapp/data/authrepo.dart';
 import 'package:coffeeshopapp/data/orderrepo.dart';
 import 'package:coffeeshopapp/firebase_options.dart';
-//import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart'; // Обновите импорт
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -20,41 +18,41 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  GetIt.I.registerSingleton<AuthRepository>(AuthRepository(app));
-  GetIt.I.registerSingleton<OrderRepository>(
-      OrderRepository(authRepository: GetIt.I.get<AuthRepository>()));
-  SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
-  runApp(
-    // DevicePreview(
-    // enabled: !kReleaseMode,
-    // builder: (context) =>
-    MyApp(),
-    //)
+  // Инициализация Firebase Analytics
+  FirebaseAnalytics analytics = FirebaseAnalytics.instance; // Используем instance
+
+  // Регистрация зависимостей с Firebase Analytics
+  GetIt.I.registerSingleton<AuthRepository>(
+    AuthRepository(app, analytics: analytics),
   );
+  GetIt.I.registerSingleton<OrderRepository>(
+    OrderRepository(authRepository: GetIt.I.get<AuthRepository>()),
+  );
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-        providers: [
-          BlocProvider<CartBloc>(
-            create: (BuildContext context) => CartBloc(),
-          ),
-          BlocProvider<OrderBloc>(
-            create: (BuildContext context) =>
-                OrderBloc(GetIt.I.get<OrderRepository>()),
-          ),
-        ],
-        child: MaterialApp(
-          // useInheritedMediaQuery: true,
-          // locale: DevicePreview.locale(context),
-          // builder: DevicePreview.appBuilder,
-          title: 'Flutter Demo',
-          theme: ThemeData(),
-          initialRoute: "/",
-          onGenerateRoute: RouteGenerator.onGenRoute,
-        ));
+      providers: [
+        BlocProvider<CartBloc>(
+          create: (BuildContext context) => CartBloc(),
+        ),
+        BlocProvider<OrderBloc>(
+          create: (BuildContext context) =>
+              OrderBloc(GetIt.I.get<OrderRepository>()),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(),
+        initialRoute: "/",
+        onGenerateRoute: RouteGenerator.onGenRoute,
+      ),
+    );
   }
 }
